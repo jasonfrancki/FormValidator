4b83542b102e527d49da551c456c59f80cfe580e
Jason Francki <jasonfrancki@gmail.com>
2020-08-13 21:52:49 -0500

    refactored required field checks

--- app.js
+++ app.js
@@ -24,21 +24,18 @@ function isValidEmail(email) {
 	return re.test(String(email).toLowerCase());
 }
 
+// Check required fields
+function checkRequired(inputArr) {
+	inputArr.forEach((input) => {
+		if (input.value === '') {
+			input.parentElement.className = 'form-control error';
+		}
+	});
+}
+
 // Event Listeners
 form.addEventListener('submit', (e) => {
 	e.preventDefault();
 
-	if (username.value === '') {
-		showError(username, 'Username is required');
-	} else {
-		showSuccess(username);
-	}
-
-	if (email.value === '') {
-		showError(email, 'Email is required');
-	} else if (!isValidEmail(email.value)) {
-		showError(email, 'Email is not valid');
-	} else {
-		showSuccess(email);
-	}
+	checkRequired([ username, email, password, password2 ]);
 });
